77f4046e620e2894593b3b0843bfac1d1f1ed2c9
Michael Kratzer <mkratzer@mytum.de>
2016-08-17 13:06:22 +0200

    Project Setup

---
+++ PCRemoteControl/PCRemoteControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PCRemoteControl
+//
+//  Created by Michael Kratzer on 29/07/16.
+//  Copyright © 2016 Michael Kratzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
